250dc88d1c9979e7ad2c34fba4159146950d7422
yahohulia <yaroslavhohulia@gmail.com>
2025-12-03 17:10:37 +0200

    added generics to functions values

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items, key): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   for (const item of items) {
